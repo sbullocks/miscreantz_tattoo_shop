6bd12365863d723e3f087ce30327bb7895c2586d
sbullocks <sbullocks@gmail.com>
2023-01-16 15:28:08 -0500

    added routes for appointments to create, retrieve, update and delete.

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/appointments', (req, res) => {
+    // handle creating a new appointment
+});
+
+app.get('/appointments/:id', (req, res) => {
+    // handle retrieving an existing appointment
+});
+
+app.put('/appointments/:id', (req, res) => {
+    // handle updating an existing appointment
+});
+
+app.delete('/appointments/:id', (req, res) => {
+    // handle deleting an existing appointment
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
