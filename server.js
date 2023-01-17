e664fe5162f2c147ca0317f5e4b95cf9ed889590
sbullocks <sbullocks@gmail.com>
2023-01-16 20:22:01 -0500

    added dotenv.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const { AppointmentSchema, ArtistSchema, Appointment, Artist } = require('./models');
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,7 +48,16 @@ app.post('/appointments', (req, res) => {
 });
 
 
-mongoose.connect('mongodb+srv://<username>:<password>@cluster.mongodb.net/<dbname>', {
+// mongoose.connect('mongodb+srv://<username>:<password>@cluster.mongodb.net/<dbname>', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch(err => {
+//     console.log('Error connecting to MongoDB:', err);
+// });
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster.mongodb.net/${process.env.MONGO_DBNAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
