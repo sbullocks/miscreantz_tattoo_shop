d468bd40c47ceb91004347abd1761279996c3cb0
sbullocks <sbullocks@gmail.com>
2023-01-16 20:26:55 -0500

    updated models.

--- models.js
+++ models.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const Appointment = mongoose.model('Appointment', AppointmentSchema);
-const Artist = mongoose.model('Artist', ArtistSchema);
-
 
 const AppointmentSchema = new mongoose.Schema({
     artistId: { type: String, required: true },
@@ -9,11 +6,13 @@ const AppointmentSchema = new mongoose.Schema({
     date: { type: Date, required: true },
     time: { type: String, required: true },
 });
-
 const ArtistSchema = new mongoose.Schema({
     name: { type: String, required: true },
     portfolio: [{ type: String }],
 });
+
+const Appointment = mongoose.model('Appointment', AppointmentSchema, 'appointments');
+const Artist = mongoose.model('Artist', ArtistSchema, 'artists');
 
 module.exports = {
     AppointmentSchema,
